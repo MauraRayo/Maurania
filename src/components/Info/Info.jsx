55f3b9c6733a3e70b68f09f0ca9e0c4293300ca2
MauraRayo <rayomaura@gmail.com>
2024-01-19 13:08:10 +0100

    pdf - modificacionlinks

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -9,13 +9,13 @@ export const Info = () =>{
             <div className={styles.content}>
                 <h2 className={styles.title} >Hi, Soy Maurania</h2>
                 <p className={styles.trabajo}>Frontend Developer en <a href="https://www.jobbe.es/wp/">Jobbe ETT</a></p>
-                <div className={styles.contacotBtn}>
+                <div className={styles.contactoBtn}>
                     <a href="mailto:maurarayo3@gmail.com" > <img src={getImageUrl("contact/emailIcon.png")} alt="maurarayo3@gmail.com" /></a>
                     <a href="https://www.linkedin.com/in/maurarayo/" target="_black"> <img src={getImageUrl("contact/linkedinIcon.png")} alt="in/maurarayo/"/></a>
                     <a href="https://github.com/MauraRayo" target="_black"> <img src={getImageUrl("contact/githubIcon.png")} alt="github.com/MauraRayo" /></a>
                 </div>
                 <button className={styles.contactBtn}>
-                    <a href="/assets/contact/CVMauraniaRayo.pdf" download="CV_MauraniaRayo" > Descargar CV </a>
+                    <a href="/assets/contact/CVMauraniaRayo.pdf" download="CVMauraniaRayo" > Descargar CV </a>
                 </button>
             </div>
             <img src={getImageUrl("hero/heroImage.png")} alt="Personaje"  className={styles.heroImg}/>
