830b237a39b6f5c094fed4a6d7de9963668360b1
MauraRayo <56032847+MauraRayo@users.noreply.github.com>
2024-03-05 19:29:14 +0100

    change

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, demo, date, source },
 }) => {
   return (
     <div className={styles.container}>
@@ -16,7 +16,8 @@ export const ProjectCard = ({
           />
         <div className={styles.conta}>
           <h3 className={styles.title}>{title}</h3>
-          <p className={styles.description}>{description}</p>
+          <p className={styles.description}>{description} </p>
+          <p className={styles.date}>{date} </p>
           <ul className={styles.skills}>
             {skills.map((skill, id) => {
               return (
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./Projects.module.css";
+import styles from "./projects.module.css";
 
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
